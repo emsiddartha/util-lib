55e55fe69430770868606e4353a7bd586cef65e6
Siddartha <sid.errabolu@gmail.com>
2019-09-09 04:30:52 -0400

    charToString added in Stringutil

--- src/main/scala/org/bheaver/ngl4/util/StringUtil.scala
+++ src/main/scala/org/bheaver/ngl4/util/StringUtil.scala
@@ -4,4 +4,6 @@ object StringUtil {
   def isEmpty(x: String) = x == null || x.isEmpty
 
   def isEmptyTrim(x: String) = x == null || x.trim.isEmpty
+
+  def charToString(x: Char) = if(x==null || x == ' ') "" else x.toString.trim
 }
